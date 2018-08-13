3592f341e068b78f9933fa66d2856d237a8f020b
Nikolay Khodyunya <nickolayho@gmail.com>
2018-08-13 19:17:36 +0700

    Remove orphaned code

--- ClientApp/src/app/users/users.component.ts
+++ ClientApp/src/app/users/users.component.ts
@@ -12,16 +12,4 @@ import { UserService } from '../_services/user.service';
     constructor(private userService: UserService) {
       this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
-
-    ngOnInit() {
-      this.loadAllUsers();
-    }
-
-    deleteUser(id: number) {
-        this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
-    }
-
-    private loadAllUsers() {
-      this.userService.getAll().subscribe(users => { this.users = users; });
-    }
   }
